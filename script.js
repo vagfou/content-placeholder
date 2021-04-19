11c3a1a52ec6520aa4097cf39ccec90d0f0dfc3f
vagfou <foufoulas.evangelos@protonmail.com>
2021-04-19 18:46:13 +0300

    project done

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ const date = document.getElementById('date')
 
 const animated_bgs = document.querySelectorAll('.animated-bg')
 const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 2000)
+
+function getData () {
+    header.innerHTML = '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" alt="" >'
+    title.innerHTML = 'Lorem ipsum dolor sit amet.'
+    excerpt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, error!'
+    profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" >'
+    name.innerHTML = 'John Doe'
+    date.innerHTML = 'Oct 98m 2020'
+
+    animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
+    animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
+}
